afc5361705649391871474ba505297092938f3a3
Tero Keränen <tero-keranen@hotmail.com>
2025-10-31 10:16:43 +0200

    Ensimmäinen esimerkki!

---
+++ H0_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string fname;
+    cout << "Anna etunimesi:" << endl;
+    cin>>fname;
+    cout << "Terve "<<fname <<endl;
+
+    return 0;
+}
